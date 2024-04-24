e1afa85bd9da8cd6661fbef4edf1fb5c8c3d4060
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-04-24 07:20:19 +0700

    fix CORS vercel

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "https://datn-admin-be.onrender.com",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
