580d37080fd9701941f45e87a42a32547c7fa1e1
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-05-13 15:57:12 +0700

    Simple manage reviews

--- src/apis/reviews.tsx
+++ src/apis/reviews.tsx
@@ -1,8 +1,30 @@
+import { Dish } from "./dishes";
+import { User } from "./users";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 export interface Review {
   id: number;
-  dishId: number;
-  userId: number;
   content: string;
   createdAt: string;
   updatedAt: string;
+  user: User;
+  dish: Dish;
+}
+export async function getAllReviewData(): Promise<Review[]> {
+  try {
+    const response = await fetch(`${apiUrl}/reviews`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error getting data:", error);
+    throw error;
+  }
 }
